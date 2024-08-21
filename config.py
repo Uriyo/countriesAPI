a2b76953167b16eaec8433fa1b2d5a32348f413f
Kushagra Gangwar <kgang6434@gmail.com>
2024-08-21 13:48:25 +0530

    database updates

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres_user:postgres_password@localhost:5432/countries_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://default:AElZVWcDj6p5@ep-calm-butterfly-a7bus4ae.ap-southeast-2.aws.neon.tech:5432/verceldb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
     PORT=5000
+
+# postgresql://postgres_user:postgres_password@localhost:5432/countries_db
